381aa8a5c4f1c8d1e7f1aebc296e0422be79cbf3
Ivan Prodeus <learning.webpractice@gmail.com>
2022-10-25 21:31:32 +0300

    redid the header again now ok

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,13 +1,7 @@
 import { Header } from "./Searchbar/Searchbar";
-import { Gallery } from "./Gallery/ImageGallery";
+
 
 
 export const App = () => {
-  return (
-    <>
-      <Header />
-      <Gallery />
-    </>
-
-  );
+  return <Header/>;
 };
---
+++ src/components/Searchbar/Searchbar.jsx
@@ -0,0 +1,25 @@
+
+import {
+    ButtonSubmit, SearchBox, Head, ButtonIcon, 
+    ButtonText,
+    SearchField
+} from "./Searchbar.styled";
+
+export const Header = () => {
+    return (
+        <Head>
+            <SearchBox class="form">
+                <ButtonSubmit type="submit" class="button">
+                    <ButtonIcon/>
+                </ButtonSubmit>
+
+                <SearchField
+                    type="text"
+                    autocomplete="off"
+                    autofocus
+                    placeholder="Search images and photos"
+                />
+            </SearchBox>
+        </Head>
+    );
+};
